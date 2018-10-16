8f37b470d3634ee5eb7ebbe4c00a78e7a2f2d6ed
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-10-16 11:37:49 +0100

    Add AddItem action method

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -31,5 +31,22 @@ namespace AspNetCoreTodo.Controllers
             // Render view using the model
             return View(model);
         }
+
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddItem(TodoItem newItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var successful = await _todoItemService.AddItemAsync(newItem);
+            if (!successful)
+            {
+                return BadRequest(new { error = "Could not add item." });
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
